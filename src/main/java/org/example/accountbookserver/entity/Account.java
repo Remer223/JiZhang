6de8e9c8b83a8b417bce75662bdafd6833f0a1d6
lisusiluofu <1954478031@qq.com>
2026-05-27 00:41:27 +0800

    接触7

--- src/main/java/org/example/accountbookserver/entity/Account.java
+++ src/main/java/org/example/accountbookserver/entity/Account.java
@@ -11,7 +11,7 @@ public class Account {
     private String category;
     private String remark;
     private String create_time;
-    //2
+    //221e312
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getUser_id() { return user_id; }
